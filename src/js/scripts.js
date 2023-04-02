dd359e8a5576e5b9d6bc225f4e6460fd0d96342d
Yula <ivanykjula@gmail.com>
2023-04-02 11:19:20 +0300

    Update 2023-04-02T08:18:42.370Z

--- src/js/scripts.js
+++
@@ -1,6 +0,0 @@
-// Custom Scripts
-@@include('./libs/swiper(lib).js')
-@@include('./libs/gsap(lib).min.js')
-@@include('./libs/ScrollTrigger(lib).min.js')
-@@include('main.js')
-@@include('specialities.js')
